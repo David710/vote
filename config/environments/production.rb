8df76a33a88edfb87a8eebb6538ae1120ff58c15
David710 <bednarski_david@hotmail.com>
2016-08-09 21:34:42 +0200

    retour

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,9 +35,9 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
-   config.action_cable.url = 'wss://seminairegp.herokuapp.com/cable'
-   config.action_cable.allowed_request_origins = [ 'https://seminairegp.herokuapp.com', /http:\/\/seminairegp.herokuapp.*/ ]
+   config.action_cable.mount_path = nil
+#   config.action_cable.url = 'wss://seminairegp.herokuapp.com/cable'
+#   config.action_cable.allowed_request_origins = [ 'https://seminairegp.herokuapp.com', /http:\/\/seminairegp.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
